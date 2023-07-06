b6292f6447a7a0c78d020d99b4418e6c2f74184f
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-06 12:29:03 +0100

    Home page configuration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,6 +15,10 @@ const Home = () => {
         //redirect to Home
     }
 
+    const handleLogoutClick = () => {
+        //Asks a user a confirmation and logouts
+    }
+
     const handleProfileClick = () => {
         setShowProfile(true);
     }
@@ -23,12 +27,58 @@ const Home = () => {
         setShowProfile(!showProfile);
     }
 
+    /** TOPIC */
+
+    const [topics, setTopics] = useState([
+        {
+             id: 1,
+             name: "Java",
+            numberOfQuestions: 10
+        },
+        {
+             id: 2,
+             name : "Algorithms",
+             numberOfQuestions: 2
+        },
+        {
+             id: 3,
+             name : "Data Structures and Algorithms",
+             numberOfQuestions: 2
+        },
+        {
+             id: 4,
+             name : "Data Structures and Algorithms",
+             numberOfQuestions: 2
+        },
+        {
+             id: 5,
+             name : "Software Development Advanced Techniques",
+             numberOfQuestions: 2
+        },  
+        {
+            id: 6,
+            name : "Project and Dissertation for MSc Computer Science",
+            numberOfQuestions: 2
+       }  
+  
+    ]);
+
+    const topicElements = topics.map(topic => <Topic key={topic.id} id={topic.id} name={topic.name} numberOfQuestions={topic.numberOfQuestions}/>)
+
+
+
 
     return (
         <div className="home-page">
-            <Header options={[{ label: 'Home', action: handleHomeClick }, { label: 'Profile', Icon: FaUser, action: handleProfileClick }]} showProfile={showProfile} toggleProfile={toggleProfile} />
+            <Header options={[{ label: 'Home', action: handleHomeClick }, { label: 'Profile', Icon: FaUser, action: handleProfileClick }, {label: 'Logout', action: handleLogoutClick}]} showProfile={showProfile} toggleProfile={toggleProfile} />
             {showProfile && <ProfileCard />}
-            <Topic />
+            <div className="home-content">
+                <h1>Welcome, User!</h1>
+                <h2>Ready for a quiz?</h2>
+                <div className="topics-list">
+                    {topicElements}
+                </div>
+            </div>
             <Footer />
         </div>
     )
