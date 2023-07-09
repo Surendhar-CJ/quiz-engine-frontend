eaac1e0905da5fcb1f38d3caffe18cc797cd4e54
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-10 00:08:01 +0100

    Configured feedback

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -5,6 +5,7 @@ import Modal from "../components/Modal";
 import TestHeader from "../components/TestHeader";
 import StartQuiz from '../components/StartQuiz';
 import QuizQuestion from '../components/QuizQuestion';
+import FeedbackCard from '../components/FeedbackCard';
 import TestFooter from '../components/TestFooter';
 import "../styles/Quiz.css";
 
@@ -17,6 +18,9 @@ const Quiz = () => {
 
     const [showQuizStarted, setShowQuizStarted] = useState(false);
     const [quizQuestion, setQuizQuestion] = useState(null);
+    const [questionCount, setQuestionCount] = useState(0);
+    const [feedback, setFeedback] = useState(null);
+    const [isPreviousQuestionFeedbackShown, setIsPreviousQuestionFeddbackShown] = useState(true);
 
         
     const handleOnStartQuizClick = () => {
@@ -49,7 +53,7 @@ const Quiz = () => {
             const data = await response.json();
             if(response.ok) {
                 setQuizQuestion(data);
-                console.log(data);
+                setQuestionCount(questionCount + 1);
             }
 
         } catch (error) {
@@ -57,35 +61,6 @@ const Quiz = () => {
         }
     } ;
 
-
-    const getQuestion = async() => {
-        try {
-            const response = await fetch("http://localhost:9090/api/v1/quizzes/quiz-questions", {
-                method : "POST",
-                headers : {
-                    "Content-Type" : "application/json"
-                },
-                body: JSON.stringify({
-                    quizId: contextValue.quizDetails.id,
-                    topicId: contextValue.topic.id
-                })
-                
-            }); 
-            console.log({
-                quizId: contextValue.quizDetails.id,
-                topicId: contextValue.topic.id
-            })
-            const data = await response.json();
-            if(response.ok) {
-                setQuizQuestion(data);
-                console.log(data);
-            }
-
-        } catch (error) {
-            console.error("Error :", error);
-        }
-
-    };
 
     const handleOnClickNext = async (selectedChoices) => {
         try {
@@ -103,9 +78,11 @@ const Quiz = () => {
             }); 
 
             const data = await response.json();
+
             if(response.ok) {
                 setQuizQuestion(data.questionDTO);
-                console.log(data.questionDTO);
+                setFeedback(data.feedbackResponse);
+                setQuestionCount(questionCount + 1);
             }
 
         } catch (error) {
@@ -113,26 +90,45 @@ const Quiz = () => {
         }
     }
 
-
+    /*
+        (1, 'IMMEDIATE_RESPONSE'),
+        (2, 'IMMEDIATE_CORRECT_ANSWER_RESPONSE'),
+        (3, 'IMMEDIATE_ELABORATED'),
+        (4, 'DELAYED_RESPONSE'),
+        (5, 'DELAYED_CORRECT_ANSWER_RESPONSE'),
+        (6, 'DELAYED_ELABORATED');
+    */
 
     return (
         <div className = "test-page">
             <TestHeader />
             <div className="quiz-area">
-            {showQuizStarted && quizQuestion? 
-                <QuizQuestion 
-                id={quizQuestion.id} 
-                text={quizQuestion.text} 
-                score={quizQuestion.score}
-                type={quizQuestion.questionType}
-                choices={quizQuestion.choices} 
-                onClickNext={handleOnClickNext}                  
-                /> 
+                
+            {showQuizStarted && quizQuestion ? 
+                <div>
+                    <div className="question-number">{questionCount}/{contextValue.topic.numberOfQuestions}</div>
+                    <QuizQuestion 
+                    id={quizQuestion.id} 
+                    text={quizQuestion.text} 
+                    score={quizQuestion.score}
+                    type={quizQuestion.questionType}
+                    choices={quizQuestion.choices} 
+                    onClickNext={handleOnClickNext}                  
+                    /> 
+                </div>
                 : 
                 <Modal className={showQuizStarted ? '' : 'visible'} show={showQuizStarted}>
                     <StartQuiz onStartQuizClick ={handleOnStartQuizClick}/>
                 </Modal> 
             }
+
+            {feedback !== null && 
+                <FeedbackCard result ={feedback.result} 
+                correctAnswer={feedback.correctAnswer} 
+                explanation={feedback.explanation} 
+            />}
+            
+
             </div>
             <TestFooter />
         </div>   
