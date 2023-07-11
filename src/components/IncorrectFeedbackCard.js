e04a77d4758fb5f923640121118b6bb737b3004b
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-11 21:10:05 +0100

    Configured QuizResult and added QuizDetailedResults

---
+++ src/components/IncorrectFeedbackCard.js
@@ -0,0 +1,19 @@
+
+
+
+const IncorrectFeedbackCard = (props) => {
+    return (
+        <div className="incorrect-feedback-card">
+            <h4>Question {props.questionNumber}</h4>
+            <p>{props.question}</p>
+            <h4>Your Answer</h4>
+            <p>{props.userAnswer}</p>
+            <h4>Correct Answer</h4>
+            <p>{props.correctAnswer}</p>
+            <h4>Explanation</h4>
+            <p>{props.explanation}</p>
+        </div>
+    )
+}
+
+export default IncorrectFeedbackCard;
--- src/context/QuizContext.js
+++ src/context/QuizContext.js
@@ -5,12 +5,13 @@ const QuizContext = createContext("");
 
 const QuizContextProvider = ({children}) => {
 
-    const[ user, setUser] = useState(null);
-    const[ topic, setTopic] = useState(null);
+    const [user, setUser] = useState(null);
+    const [topic, setTopic] = useState(null);
     const [quizDetails, setQuizDetails] = useState(null);
+    const [quizResult, setQuizResult] = useState(null);
 
     return (
-        <QuizContext.Provider value = {{user, setUser, topic, setTopic, quizDetails, setQuizDetails}}>
+        <QuizContext.Provider value = {{user, setUser, topic, setTopic, quizDetails, setQuizDetails, quizResult, setQuizResult}}>
             {children}
         </QuizContext.Provider>
     )
