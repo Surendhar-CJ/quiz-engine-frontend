a7251a953f79812d80d07fd076e8500619f4c9ef
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-19 14:57:19 +0100

    Added PrivateRoute

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import QuizDetailedResults from './pages/QuizDetailedResults.js';
 import UserProfile from './pages/UserProfile.js';
 import QuizQuestionAddition from './pages/QuizQuestionAddition.js';
+import PrivateRoute from './components/PrivateRoute.js';
 
 
 const App = () => {
@@ -18,17 +19,17 @@ const App = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<QuizResult />} />
-            <Route path="/detailed-results" element={<QuizDetailedResults />} />
-            <Route path="/profile" element={<UserProfile />} />
-            <Route path="/add-question" element ={<QuizQuestionAddition />} />
+            <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+            <Route path="/quiz" element={<PrivateRoute><Quiz /></PrivateRoute>} />  
+            <Route path="/result" element={<PrivateRoute><QuizResult /></PrivateRoute>} />  
+            <Route path="/detailed-results" element={<PrivateRoute><QuizDetailedResults /></PrivateRoute>} /> 
+            <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+            <Route path="/add-question" element={<PrivateRoute><QuizQuestionAddition /></PrivateRoute>} />  
           </Routes>
         </BrowserRouter>
       </QuizContextProvider>
     </div>
-    ); 
+  ); 
 }
 
 export default App;
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const token = localStorage.getItem('token');
+
+  return user && token ? children : <Navigate to="/" replace />;
+};
+
+export default PrivateRoute;
