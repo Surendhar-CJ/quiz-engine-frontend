525871364f3cbca30760cade1bd2907671a3aec5
Surendhar CJ <surendharcjayapal@gmail.com>
2023-07-23 23:01:13 +0100

    Dashboard initial commit

---
+++ src/components/dashboard/Sidebar.js
@@ -0,0 +1,13 @@
+import '../../styles/dashboard/Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <p>Profile</p>
+            <p>Performance Analysis</p>
+            <p>Quiz History</p>
+        </div>
+    )
+}
+
+export default Sidebar;
