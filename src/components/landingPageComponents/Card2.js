cc28029979500571fcac2237841c395b59399407
Surendhar CJ <surendharcjayapal@gmail.com>
2024-01-18 20:42:32 +0000

    Modified landing cards

--- src/components/landingPageComponents/Card2.js
+++ src/components/landingPageComponents/Card2.js
@@ -5,11 +5,13 @@ import '../../styles/LandingPageComponents.css';
 
 const Card2 = () => {
     return (
-        <div className ="card2">
-            <figure className="feedback-image">
-                <img src={myImage} alt="Feedback" style={{borderRadius: '20px',width: '700px', boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.4)'}} />
-            </figure>
-            <p>Get feedback on your performance</p>
+        <div className ="card2 card2-stack">
+            <div className="content-wrapper">
+                <figure className="feedback-image">
+                    <img src={myImage} alt="Feedback" style={{borderRadius: '20px',width: '700px', boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.4)'}} />
+                </figure>
+                <p>Get feedback on your performance</p>
+            </div>
         </div>
     )
 }
--- src/components/landingPageComponents/Card3.js
+++ src/components/landingPageComponents/Card3.js
@@ -5,10 +5,12 @@ import '../../styles/LandingPageComponents.css';
 const Card3 = () => {
     return (
         <div className="card3">
-            <p>Track your progress and much more</p>
-            <figure className="performance-image1">
-                <img src={myImage1} alt="Performance Analysis 1" style={{borderRadius: '20px',width: '700px', boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.4)'}} />
-            </figure>
+            <div className="content-wrapper">
+                <p>Track your progress and much more</p>
+                <figure className="performance-image1">
+                    <img src={myImage1} alt="Performance Analysis 1" style={{borderRadius: '20px',width: '700px', boxShadow: '2px 4px 4px rgba(0, 0, 0, 0.4)'}} />
+                </figure>
+            </div>
         </div>
     )
 }
